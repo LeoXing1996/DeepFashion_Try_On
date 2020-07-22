01e95dc15e81755c2037b5ea2bbd95c46f2db851
LeoXing <LeoXing1996@gmail.com>
2020-07-22 15:30:40 +0800

    add some helpful script

--- ACGPN_train/script/ddp.sh
+++ ACGPN_train/script/ddp.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES="4,5"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 cd ../
-
-python -m torch.distributed.launch --nproc_per_node=2 train.py \
-                --name ddp_test --batchSize 4 --nThreads 16 --gpu_ids 0,1 \
+python -m torch.distributed.launch --nproc_per_node=4 \
+       train.py --batchSize 8 --nThreads 16 --gpu_ids 0,1,2,3 \
+                --name DDP_Baseline \
                 --niter 40 --niter_decay 0
